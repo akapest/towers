dbfccd366a893879715134af8b716d43e1cc9dfd
Pestov Konstantin <konstpestov@gmail.com>
2014-04-21 15:57:05 +0600

    круговая вышка

--- client/views/map/Sector.js
+++ client/views/map/Sector.js
@@ -4,15 +4,15 @@
 (function(){
 
   window.Sector = function(center, towerAttrs, map, geo, raw){
-    this.parts =  new ymaps.GeoObjectCollection({}, {
+    this.parts = new ymaps.GeoObjectCollection({}, {
       draggable: false
     });
     this.raw = raw;
     this.center = center;
     this.sector = this.attrs = towerAttrs;
     this.angle = parseAngle(this.sector.angle);
-    this.angleSteps = this.sector.type == 'highway' || this.raw ?  1 : Math.floor(this.angle * 6);
-    this.gradientSteps = this.sector.type == 'highway'? 1 : 5;
+    this.angleSteps = this.sector.type == 'highway' || this.raw ? 1 : Math.floor(this.angle * 6);
+    this.gradientSteps = this.sector.type == 'highway' ? 1 : 5;
     this.geo = geo;
     this.map = map;
     this.geoObjects = map.geoObjects;
@@ -21,17 +21,51 @@
   $.extend(Sector.prototype, {
 
     render: function(){
+      if (Math.PI - this.angle < 0.01){
+        this.renderCircleTower();
+      } else {
+        this.renderSector();
+      }
+      if (!this.raw){
+        this.renderBase();
+      }
+      return this;
+    },
+
+    renderCircleTower: function(){
+      var lengthStep = this.getLengthSteps();
+      var opacity = 5;
+      var yColor = this.sector.color + digitToLetter(opacity) + '0';
+
+      for (var i = 1; i <= this.gradientSteps; i++){
+        var radius = lengthStep * i;
+        console.log(yColor)
+        var circle = new ymaps.Circle(
+            [this.center, radius],
+            {}, {
+              interactivityModel: 'default#transparent',
+              fillColor: yColor,
+              strokeColor: yColor,
+              strokeWidth: 0,
+              opacity: 0.8
+            });
+        this.parts.add(circle);
+      }
+      this.geoObjects.add(this.parts);
+    },
+
+    renderSector: function(){
       var previous = null,//triangle
-        sector = this.sector,
-        azimuth = sector.azimuth,
-        startAzimuth = azimuth - this.angle,
-        angleStep = this.angle / this.angleSteps,
-        lengthStep = sector.radius / this.gradientSteps,
+          sector = this.sector,
+          azimuth = sector.azimuth,
+          startAzimuth = azimuth - this.angle,
+          angleStep = this.angle / this.angleSteps,
+          lengthStep = this.getLengthSteps(),
 
-        part = null,
-        a, b, c, d;
+          part = null,
+          a, b, c, d;
 
-      for (var j = 0; j < this.angleSteps * 2 ; j++){
+      for (var j = 0; j < this.angleSteps * 2; j++){
 
         previous = null;
         azimuth = startAzimuth + j * angleStep;
@@ -41,26 +75,26 @@
             a = this.center;
             b = Geo.endPoint(a, azimuth, lengthStep);
             c = Geo.endPoint(a, azimuth + angleStep, lengthStep);
-            part = this.createPolygon([a,b,c], i)
+            part = this.createPolygon([a, b, c], i)
             this.first = part;
-            previous = [b,c]
+            previous = [b, c]
 
           } else {
             a = previous[0];
             b = previous[1];
             c = Geo.endPoint(a, azimuth, lengthStep);
             d = Geo.endPoint(b, azimuth + angleStep, lengthStep);
-            part = this.createPolygon([a,c,d,b], i)
-            previous = [c,d]
+            part = this.createPolygon([a, c, d, b], i)
+            previous = [c, d]
           }
           this.parts.add(part);
         }
       }
       this.geoObjects.add(this.parts);
-      if (!this.raw){
-        this.renderBase();
-      }
-      return this;
+    },
+
+    getLengthSteps: function(){
+      return this.sector.radius / this.gradientSteps;
     },
 
     remove: function(){
@@ -147,7 +181,6 @@
     })
     return result;
   }
-
 
 
 }());
